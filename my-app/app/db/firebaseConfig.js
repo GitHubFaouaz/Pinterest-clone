904162280d1e91e095ab771020ed0e991e2528aa
faouaz <faouaza@yahoo.com>
2024-07-21 18:10:52 +0200

    first commit

---
+++ my-app/app/db/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSQG-klblsRjVWivW1BHnHZewK39pa1vQ",
+  authDomain: "cloner-pinterest.firebaseapp.com",
+  projectId: "cloner-pinterest",
+  storageBucket: "cloner-pinterest.appspot.com",
+  messagingSenderId: "920588345629",
+  appId: "1:920588345629:web:3c3a29585d6fa41aa081f4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
+// creation de la db  et le storage sur le site
