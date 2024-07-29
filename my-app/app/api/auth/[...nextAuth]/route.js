098e871c464bc6565d01851df1c0260c068a9f58
faouaz <faouaza@yahoo.com>
2024-07-29 17:02:15 +0200

    nextauth

--- my-app/app/api/auth/[...nextAuth]/route.js
+++ my-app/app/api/auth/[...nextAuth]/route.js
@@ -1,27 +1,34 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
-// export const authOptions = {
-//   // Configure one or more authentication providers
+require("dotenv").config();
+// const Handlers = NextAuth({
 //   providers: [
 //     GoogleProvider({
 //       clientId: process.env.GOOGLE_CLIENT_ID,
 //       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 //     }),
 //   ],
-// };
+// });
 
-// export default NextAuth(authOptions);
-
-const Handlers = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.ID,
-      clientSecret: process.env.SECRET,
-    }),
-  ],
-});
-
-export { Handlers as GET, Handlers as POST };
+// export { Handlers as GET, Handlers as POST };
 // Vous exportez Handlers comme GET et POST, ce qui signifie que la même configuration sera utilisée pour les requêtes GET et POST
 // Route pour l'authentification avec next auth
+
+const options = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // Ajoutez d'autres fournisseurs ici
+  ],
+  // Ajoutez d'autres options de configuration ici
+};
+
+export const GET = (req, res) => {
+  return NextAuth(req, res, options);
+};
+
+export const POST = (req, res) => {
+  return NextAuth(req, res, options);
+};
--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -11,13 +11,16 @@ import { IoIosArrowDown, IoIosNotifications, IoIosSearch } from 'react-icons/io'
 import { FaRegUserCircle } from 'react-icons/fa';
 
 
-
 export default function Header() {
 
     const {data:session} = useSession();
     const router = useRouter();
     const db =  getFirestore(app)
-  
+    console.log("envId", process.env.GOOGLE_CLIENT_ID);
+    console.log("envSecret",process.env.GOOGLE_CLIENT_SECRET,);
+    console.log("envTest",process.env.test);
+
+
     //  useEffect(()=>{
     // //    saveUserInfo()
     //  },[session])
