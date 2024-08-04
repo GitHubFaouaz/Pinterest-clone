489e4efeaf3fc8cb1395c04b6f63eef170464501
faouaz <faouaza@yahoo.com>
2024-08-04 18:39:39 +0200

    userInfo

--- my-app/app/dashboard/[userId]/page.jsx
+++ my-app/app/dashboard/[userId]/page.jsx
@@ -13,25 +13,27 @@ export default function page({params}) {
 const db = getFirestore(app)
 const [userInfo,setUserInfo] = useState(null)
 
+
+const getUserInfo = async (email)=> {
+  const docRef = doc(db,"user", email) ;// on cherché le mail dans le user de la base de donnée
+  const docSnap = await getDoc(docRef);
+
+  if(docSnap.exists()){
+    const userData = docSnap.data()
+    setUserInfo(userData)
+  }else {
+    console.log("aucune information");
+  }
+}
+
 useEffect(()=>{
 if(params){
     getUserInfo(params.userId.replace("%40" ,"@"))
     // on remplace %40 qui n'est pas prit en compte par @ du mail http://localhost:3000/dashboard/faouazaaa@gmail.com
-
+    getUserInfo(email); 
 }
 },[params])
 
- const getUserInfo = async (email)=> {
-    const docRef =(db,"user", email) // on cherché le mail dans le user de la base de donnée
-    const docSnap = await getDoc(docRef);
-
-    if(docSnap.exists()){
-      const userData = docSnap.data()
-      setUserInfo(userData)
-    }else {
-      console.log("aucune information");
-    }
-  }
 
   return (
     <div className="bg-[#e9e9e9]  min-h-screen flex items-center justify-center">
