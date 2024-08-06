06708d971d6f77d8a75a8c2203aae0ea7034fd89
faouaz <faouaza@yahoo.com>
2024-08-06 19:19:34 +0200

    userInfo

--- my-app/app/dashboard/[userId]/page.jsx
+++ my-app/app/dashboard/[userId]/page.jsx
@@ -2,17 +2,30 @@
 
 import {doc,getDoc,getFirestore,setDoc} from 'firebase/firestore';
 import { useEffect, useState } from "react"
-// import app from '../../db/firebaseConfig'
-import app from '@/app/db/firebaseConfig'
+import app from '../../db/firebaseConfig'
+// import app from '@/app/db/firebaseConfig'
 import UserInfo from '../../components/UserInfo';
+import { useSession } from 'next-auth/react';
 
 
 
 
 export default function page({params}) {
 
+  const session = useSession();
+  // const { status, user} = session ;
+  console.log( 'session' , session);
+  const {user} = session; 
+  console.log(user);
+  console.log(session.data);
+//  if(status === 'authenticated') {
+//   // console.log(session.user);
+// //  return <p>Signed in as {session.user.email}</p>
+//  }
+
+ 
   const db = getFirestore(app);
-  const [userInfo, setUserInfo] = useState(null);
+  const [userInfo, setUserInfo] = useState('');
 
   const getUserInfo = async (email) => {
     const docRef = doc(db, "user", email); // Cherche le mail dans la collection "user" de la base de données
@@ -37,12 +50,14 @@ if(params){
   return (
     <div className="bg-[#e9e9e9]  min-h-screen flex items-center justify-center">
     {userInfo?(
+
     <div>
       <UserInfo userInfo = {userInfo}/>
     </div>
     ) : null}
     </div>
   )
+
 }
 // params pour recuperer le mail dans d'url
 //  l'adresse e-mail faouazaaa@gmail.com sera encodée comme faouazaaa%40gmail.com dans l'URL.
