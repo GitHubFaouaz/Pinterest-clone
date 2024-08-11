c404e4273de6be82fbf11fb6bef166beceeb4a79
faouaz <faouaza@yahoo.com>
2024-08-11 16:46:00 +0200

    prisma

--- my-app/app/components/UserInfo.jsx
+++ my-app/app/components/UserInfo.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useRouter } from 'next/router'
@@ -6,9 +7,10 @@ import React from 'react'
 export default function UserInfo({userInfo}) {
 
   console.log('userInfo' ,  userInfo);
-  // const {data:session} = useSession();
+  const {data:session} = useSession();
  const {user} = userInfo ;
   // const router = useRouter();
+
   const onLogOut = ()=> {
    signOut()
   //  router.push('/') // apres la deconnexion on envoi a la page d'acceuil 
@@ -16,6 +18,14 @@ export default function UserInfo({userInfo}) {
   return (
     <div className="bg-white flex flex-col items-center p-16 rounded-2xl shadow-md">
       <Image alt='imageProfile'  src={user.image} width={100} height={100} className='rounded-full'/>
-    </div>
-  )
-}
+      <h2 className='text-[30px] font-semibold'>{user.name}</h2>
+      <div className="flex gap-4">
+        <button className="rounded-full p-2 px-3 mt-5 font-semibold bg-grey-200">Partager</button>
+        {session?.user.email == user.email ? 
+          <button className="rounded-full p-2 px-3 mt-5 font-semibold bg-grey-200"onClick={()=> onLogOut()}>Deconnexion</button>
+        : null }
+    
+      </div>
+      </div>
+    )
+  }
