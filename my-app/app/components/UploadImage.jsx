c6a50df6c332d6ce9200a87259e7de8bfe75d423
faouaz <faouaza@yahoo.com>
2024-08-29 16:30:45 +0200

    creatObjetFile

--- my-app/app/components/UploadImage.jsx
+++ my-app/app/components/UploadImage.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 import { IoArrowUpCircleOutline } from "react-icons/io5";
 
+//On telecharge l'image    
 export default function UploadImage({setFile}) {
   const [selectFile,setSelectFile] =useState()
   
+  useEffect(() => {
+    return () => {
+      if (selectFile) {
+        URL.revokeObjectURL(selectFile);
+      }
+    };
+  }, [selectFile]);
   return (
     <div className='h-[450px] bg-[e9e9e9] rounded-lg'>
       <label htmlFor="" className="m-5 flex flex-col justify-center items-center cursor-pointer h-[90%] border-[2px] border-grey-200 border-dashed rounded-lg text-grey-600">
@@ -11,11 +19,15 @@ export default function UploadImage({setFile}) {
           <IoArrowUpCircleOutline  className='text-[22px]'/>
           <h2 className='font-semibold'> cliquer pour telecharger </h2>
         </div> : null }
-        {/* {selectFile ?
+        {selectFile ?
          <img src={window.URL.createObjectURL(selectFile)} alt='select image' className='object-contain h-[90%]' height={500}  width={500}/>
-         : null}*/}
+         : null}
          <input type="file" id='dropzone-file' className=""   onChange={(e)=> {setSelectFile(e.target.files[0]) ; setFile(e.target.files[0])}}/> 
       </label>
     </div>
   )
 }
+
+// window.URL.createObjectURL(selectFile) :
+
+// Cette méthode génère une URL temporaire pour l'objet File (l'image sélectionnée). Cette URL est ensuite utilisée dans l'attribut src de la balise <img> pour afficher un aperçu de l'image sélectionnée avant qu'elle ne soit réellement téléchargée.
