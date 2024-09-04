c26432199fd1f7ab59327eceaf7e6265177f1f20
faouaz <faouaza@yahoo.com>
2024-09-04 20:02:19 +0200

    onesave fonction

--- my-app/app/components/FormAdd.jsx
+++ my-app/app/components/FormAdd.jsx
@@ -5,6 +5,8 @@ import UploadImage from "./UploadImage"
 import { useSession } from 'next-auth/react';
 import { useRouter } from "next/navigation";
 import UserTag from "./UserTag";
+import onSave from '../utils/onsave'
+
 
 
 export default function FormAdd() {
@@ -19,25 +21,12 @@ export default function FormAdd() {
   const router = useRouter()
   const data = Date.now().toString()
 
-  const onSave = ()=> {
-    setLoading(true)
-    uploadFile()
-  }
-  const uploadFile = ()=> {
-  const postData  = {
-      title : titlePost,
-      desc : descPost ,
-      link  : linkPost , 
-      image: fileImage ,
-      userName : session.user.name,
-      userEmail : session.user.email,
-      userImage : session.user.image
-      // id : id 
-  }
-  }
+  // const onSave = ()=> {
+  //   // setLoading(true)
+  //   // uploadFile()
+  // }
+
   return (
-    // <button onClick={(e)=> console.log(e.target.tagName)}>test</button>
-    
     <div className="bg-white p-16 rounded-2xl">
      <div className="flex justify-end md-6">
       <button onClick={()=> onSave()}  className="bg-red-500 p-2 text-white px-3 font-semibold rounded-lg">
@@ -51,7 +40,7 @@ export default function FormAdd() {
       <UploadImage setFile={(file)=> setFileImage(file)}/>{/* on recupère les images du props setFile */} 
         <div className="col-span-2">
           <div className="w-[100%]">
-            <input type="text" onChange={(e)=>setTitle(e.target.value)} placeholder="Ajouter un titre" className="text-[35px] outline-none font-bold w-ful border-b-[2px] border-gray-400 placehorder-gray-400" />
+            <input type="text" onChange={(e)=>setTitle(e.target.value)} placeholder="Ajouter un titre" className="text-[35px] outline-none font-bold w-full border-b-[2px] border-gray-400 placeholder-gray-400" />
             <h2 className="text-[12px] text-gray-400 w-full wb-8"> Les premiers 40 caractères sont ce qui apparaissent dans le flux </h2>
             <UserTag  user={session?.user}/>
             <textarea onChange={(e)=> setDesc(e.target.value)} placeholder="Ajouter une description"  className="w-full pb-4 mt-8 outline-none text-[14px] border-gray-400 border-b-[2px] placeholder-gray-400" cols={30}></textarea>
