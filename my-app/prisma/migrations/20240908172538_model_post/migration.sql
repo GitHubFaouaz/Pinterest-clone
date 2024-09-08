196b328d5fe733c7d67cbce5f06712a14e085d22
faouaz <faouaza@yahoo.com>
2024-09-08 19:27:15 +0200

    prisma migrate

---
+++ my-app/prisma/migrations/20240908172538_model_post/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "userNamee" TEXT NOT NULL,
+    "userEmail" TEXT NOT NULL,
+    "userImage" TEXT NOT NULL
+);
