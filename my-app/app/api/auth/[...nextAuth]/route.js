3a15aaee681b35f35f9f79668ad733b8642894e0
faouaz <faouaza@yahoo.com>
2024-09-16 17:00:50 +0200

    methode post

--- my-app/app/api/auth/[...nextAuth]/route.js
+++ my-app/app/api/auth/[...nextAuth]/route.js
@@ -46,24 +46,3 @@ const Handlers = NextAuth({
 });
 
 export { Handlers as GET, Handlers as POST };
-// Vous exportez Handlers comme GET et POST, ce qui signifie que la même configuration sera utilisée pour les requêtes GET et POST
-// Route pour l'authentification avec next auth
-
-// const options = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//     // Ajoutez d'autres fournisseurs ici
-//   ],
-//   // Ajoutez d'autres options de configuration ici
-// };
-
-// export const GET = (req, res) => {
-//   return NextAuth(req, res, options);
-// };
-
-// export const POST = (req, res) => {
-//   return NextAuth(req, res, options);
-// };
